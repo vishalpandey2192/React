dce4ee2bcf0803c8e0cb8d51bbb6ea23d442093d
vishalpandey <vishal.pandey@utah.edu>
2017-09-04 14:53:09 -0600

    Types of Input and State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = { term: 'Enter'};
+    }
+    render(){
+        // return <input onChange={(event)=> console.log(event.target.value)}/>;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event=> this.setState({term: event.target.value})} />
+                {/*Value of Input: {this.state.term}*/}
+            </div>
+        )
+    }
+
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+
 }
 
-export default SearchBar; changes()
+export default SearchBar;
