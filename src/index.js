0cc61d6549a90247a3b8402e847f25983840612d
vishalpandey <vishal.pandey@utah.edu>
2017-09-06 16:58:55 -0600

    Youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 
 // Create a new compenent to produce HTML
 
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyB6nQcZ4t4jS2XxOEMPADnBYr2CsE2M1AE';
 
-const App = function () {
-    return <div>
-        <SearchBar/>
-    </div>;
+class App extends Component{
+
+
+
+    constructor(props){
+        super(props);
+        this.state = {videos: [] };
+        console.log(this.state);
+
+        YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+
+            this.setState({videos});
+            console.log(videos);
+        });
+    }
+
+
+
+    render(){
+        return (<div>
+            <SearchBar/>
+        </div>);
+    }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
